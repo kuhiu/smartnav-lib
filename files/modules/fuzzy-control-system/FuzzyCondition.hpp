ffa9628e6c953fadfec623734cdb6aca03b72a96
Kuhiu <marfus@hotmail.es>
2022-12-25 22:50:54 -0300

    add FuzzyConditionPtr and input/output as argument in evaluate and update methods

--- files/modules/fuzzy-control-system/FuzzyCondition.hpp
+++ files/modules/fuzzy-control-system/FuzzyCondition.hpp
@@ -1,8 +1,11 @@
 #ifndef F29DD846_2172_450B_9CF1_993DDCF84C9F
 #define F29DD846_2172_450B_9CF1_993DDCF84C9F
 
+#include <memory>
 class FuzzyCondition {  
 public:
+  /** FuzzyCondition smart pointer */
+  using FuzzyConditionPtr = std::shared_ptr<FuzzyCondition>;
   /** FuzzyCondition constructor */
   FuzzyCondition() = default;
   /** FuzzyCondition destructor */
@@ -10,15 +13,17 @@ public:
   /**
    * @brief Evaluate any condition
    * 
-   * @return * float 
+   * @param system_io 
+   * @return float 
    */
-  virtual float evaluate() const = 0;
+  virtual float evaluate(std::shared_ptr<std::vector<FuzzyIO>> system_io) const = 0;
   /**
    * @brief Update any condition
    * 
    * @param value 
+   * @param system_io 
    */
-  virtual void update(float value) = 0;
+  virtual void update(float value, std::shared_ptr<std::vector<FuzzyIO>> system_io) = 0;
 
 };
 
