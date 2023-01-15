d930f65fc3fff4f8065520697876f2a28eaf6fe7
Kuhiu <marfus@hotmail.es>
2023-01-15 19:04:35 -0300

    some methods were removed from the header.

--- files/modules/fuzzy-control-system/FuzzyInput.hpp
+++ files/modules/fuzzy-control-system/FuzzyInput.hpp
@@ -8,70 +8,36 @@
 #include <FuzzyIO.hpp>
 #include <FuzzyMembershipTrapezoidal.hpp>
 
+#include <nlohmann/json.hpp>
+
+using namespace nlohmann;
+
 class FuzzyInput : public FuzzyIO {
 public:
   /** FuzzyInput constructor */
   FuzzyInput(const std::string &name, std::vector<std::shared_ptr<FuzzyMembership>> memberships) : 
     FuzzyIO(name, memberships) {};
   /** FuzzyInput destructor */
-  ~FuzzyInput() = default;
+  virtual ~FuzzyInput() = default;
   /**
    * @brief Input fuzzification
    * 
    */
-	void fuzzyfication(float input_value) {
-    for ( auto &membership : __memberships )
-      membership->compute_degree_of_membership(input_value);
-	};
+	void fuzzyfication(float input_value);
   /**
    * @brief Try to parse input
    * 
    * @param io input to parse
    * @return FuzzyInput 
    */
-  static FuzzyInput parse(nlohmann::json input_json) {
-    std::ostringstream err;
-    std::string name;
-    std::vector<std::shared_ptr<FuzzyMembership>> memberships;
+  static FuzzyInput parse(const nlohmann::json& input_json);
 
-    // Check if it contains the name of io
-    if(!input_json.contains(__NAME_KEY)) {
-      err << "Invalid input arguments: " << input_json.dump();
-      throw std::runtime_error(err.str());
-    }
-    // Check if it contains the membership functions
-    if(!input_json.contains(__MEMBERSHIPS_FUNCTIONS_KEY)) {
-      err << "Invalid input arguments: " << input_json.dump();
-      throw std::runtime_error(err.str());
-    }
-    // Check if name is a string
-    if(!input_json.at(__NAME_KEY).is_string()) {
-      err << "Invalid type of input name: " << input_json.at(__NAME_KEY).dump();
-      throw std::runtime_error(err.str());
-    }
-    // Check if membership vector 
-    if(!input_json.at(__MEMBERSHIPS_FUNCTIONS_KEY).is_array()) {
-      err << "Invalid type of input membership function: " << input_json.at(__MEMBERSHIPS_FUNCTIONS_KEY).dump();
-      throw std::runtime_error(err.str());
-    }
-    name = input_json.at(__NAME_KEY);
-    // Parse inputs
-    for (const auto &membership : input_json.at(__MEMBERSHIPS_FUNCTIONS_KEY)) {
-      try {
-        // Membership type 
-        if(membership.contains(FuzzyMembershipTrapezoidal::__MB_TRAPEZOIDAL_KEY)  ) {
-          auto fuzzy_membership_trapezoidal = FuzzyMembershipTrapezoidal::parse(membership);
-          memberships.push_back(fuzzy_membership_trapezoidal);
-        }
-      }
-      catch(const std::exception& e) {
-        err << "Error parsing as FuzzyInput: " << input_json.dump();
-        std::runtime_error(err.str());
-      }
-    }
-    return (FuzzyInput(name, memberships));
-  };
-
+private:
+  /** Membership function key */
+  static constexpr auto __MEMBERSHIPS_FUNCTIONS_KEY{"membership_functions"};
+  /** Name key */
+  static constexpr auto __NAME_KEY{"name"};
+  
 };
 
 
