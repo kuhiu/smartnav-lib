fd14735ab025d5782c17cb1da8e50a9454593d39
Kuhiu <marfus@hotmail.es>
2023-01-15 20:05:27 -0300

    add support for simple moving average

--- files/modules/hc-sr04/DistanceSensors.hpp
+++ files/modules/hc-sr04/DistanceSensors.hpp
@@ -6,6 +6,7 @@
 #include <thread>
 #include <vector>
 
+#include <SimpleMovingAverage.hpp>
 class DistanceSensors {
 public:
 	/** DistanceSensor constructor */
@@ -23,27 +24,17 @@ private:
 	/** Driver directory */
 	static constexpr auto __HC_SR04_DRIVER_DIR = "/dev/hcsr04";
 	/** Number of sensors */
-	const int __NRO_SENSORS = 3;
-	/** Number of samples to average */
-	int __nro_samples_to_average = 3;
+	static const int __NRO_SENSORS = 3;
+	/** One queue per sensor to calculate the sma */
+	std::vector<SimpleMovingAverage> sma_vec;
 	/** Driver file descriptor */
 	int __fd;
-	/** Distances from distance sensors */
-	std::vector<std::vector<float>> __distance_from_sensors; 
-	/** Measures guard */
-	std::mutex __measures_guard;
 	/** Reading distance sensor thread */
 	std::thread __reading_thread;
 	/** Thread status */
 	std::atomic<bool> __is_running;
 	/** Read distances from driver */
 	void __readDistance();
-	/**
-	 * @brief Average of al samples taken
-	 * 
-	 * @return std::vector<int> 
-	 */
-	std::vector<int> __averageOfDistances();
 	/**
 	 * @brief Time to distance
 	 * 
