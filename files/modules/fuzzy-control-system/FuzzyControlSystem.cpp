48cdf8555708ea7933837c2fd20ca970132eefb7
Kuhiu <marfus@hotmail.es>
2023-02-20 11:18:04 -0300

    add debug print and move methods to the srcs file

--- files/modules/fuzzy-control-system/FuzzyControlSystem.cpp
+++ files/modules/fuzzy-control-system/FuzzyControlSystem.cpp
@@ -1,5 +1,13 @@
 #include "FuzzyControlSystem.hpp"
 
+//#define DEBUG_FUZZYCONTROLSYSTEM 1
+#ifdef DEBUG_FUZZYCONTROLSYSTEM
+ 	#define DEBUG_PRINT(fmt, args...) printf( "DEBUG: %s:%d:%s(): " fmt, \
+																						__FILE__, __LINE__, __func__, ##args)
+#else
+	#define DEBUG_PRINT(fmt, args...) /* Don't do anything in release builds */
+#endif
+
 constexpr const char *FuzzyControlSystem::__FUZZY_SYSTEM_KEY;
 constexpr const char *FuzzyControlSystem::__INPUTS_KEY;
 constexpr const char *FuzzyControlSystem::__OUTPUTS_KEY;
@@ -80,4 +88,56 @@ FuzzyControlSystem FuzzyControlSystem::parse(const nlohmann::json& fuzzy_control
     }
   }
   return FuzzyControlSystem(system_inputs, system_outputs, system_rules);   
+}
+
+std::vector<FuzzyOutput> FuzzyControlSystem::evaluate(std::vector<std::pair<std::string, float>> inputs_to_update) {
+  bool found = false;
+
+  // Check input name 
+  for (auto &system_input : __system_inputs) {
+    found = false;
+    for (auto &input_to_update : inputs_to_update) {
+      DEBUG_PRINT("Inputs name: %s - %s.\n", system_input.getName().c_str(), input_to_update.first.c_str());
+      if (system_input.getName() == input_to_update.first) {
+        // Input fuzzification
+        system_input.fuzzyfication(input_to_update.second);	
+        found = true;
+      }
+    }
+    if (found == false) {
+      std::stringstream err;
+      err << "Input not defined in the fuzzy system.";
+      throw std::runtime_error(err.str().c_str());
+    }
+  }
+  // Test
+  for (auto &system_input : __system_inputs) {
+    DEBUG_PRINT("Input name: %s.\n", system_input.getName().c_str());
+    for (auto &system_membership : system_input.getMemberships()) {
+      DEBUG_PRINT("Membership values: %f.\n", system_membership->getValue());
+    }
+  }
+
+  // Before evaluate clean all membership functions of each output
+  for (auto &system_output : __system_outputs)
+    system_output.resetMembershipFuntions();
+
+  // Rules evaluation
+  for (auto &rule : __system_rules) 
+    rule.evaluate(__system_inputs, __system_outputs);
+
+  // Test
+  for (auto &system_output : __system_outputs) {
+    DEBUG_PRINT("Output name: %s.\n", system_output.getName().c_str());
+    for (auto &system_membership : system_output.getMemberships()) {
+      DEBUG_PRINT("Membership values: %f.\n", system_membership->getValue());
+    }
+  }
+
+  // Output deffuzification
+  for ( auto &output : __system_outputs ) {
+    output.defuzzification();
+  }
+
+  return __system_outputs;
 }
