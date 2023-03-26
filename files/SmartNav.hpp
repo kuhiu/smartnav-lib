3beddbdaedcc1a2e04226545bee522860bcdd6e1
Kuhiu <marfus@hotmail.es>
2023-03-26 12:48:19 -0300

    add isRunning, stop methods. newTarget method was added to add on runtime all targets you want. Add support to the all new modules and apps. A new privated method __visualization was added to recognize new object using convolutional neural networks. A new __arrivation method was added to check if the target was reached. A new __getRelatuveTargetPos method was added to get the relative target position to the current position. whereHaveToGo method was added to get the angle where the robot have to go to reach the target. getClosestPosition method was added to get the closest point of the object

--- files/SmartNav.hpp
+++ files/SmartNav.hpp
@@ -1,17 +1,30 @@
 #ifndef A75E67F7_9C3F_4370_A541_4B1DB5BD2F02
 #define A75E67F7_9C3F_4370_A541_4B1DB5BD2F02
 
+#include <chrono>
 #include <fstream>
 #include <iostream>
 #include <memory>
 #include <thread>
+#include <vector>
 
 #include <CaptureFrame.hpp>
-#include <Compass.hpp>
 #include <Driver.hpp>
 #include <DistanceSensors.hpp>
 #include <FuzzyControlSystem.hpp>
 #include <Headlights.hpp>
+#include <PositionEstimator.hpp>
+#include <utilities.hpp>
+#include <DecayGraph.hpp>
+#include <FrameProcessor.hpp>
+#include <Obstacle.hpp>
+#include <CartesionPosition.hpp>
+#include <PolarPosition.hpp>
+#include <Buttons.hpp>
+#include <SmartEvasion.hpp>
+#include <SmartLights.hpp>
+#include <Tracker.hpp>
+#include <Reporter.hpp>
 
 class SmartNav {
 public:
@@ -19,10 +32,30 @@ public:
   SmartNav();
   /** Smartnav destructor */
   ~SmartNav();
+  /**
+   * @brief The thread is running 
+   * 
+   * @return true 
+   * @return false 
+   */
+  bool isRunning() const { return __is_running; };
+  /**
+   * @brief Stop the robot
+   * 
+   */
+  void stop() { __is_running = true; };
+  /**
+   * @brief Add a new target 
+   * 
+   * @param arrival_point 
+   */
+  void newTarget(CartesianPosition arrival_point) {
+    __tracker->addTarget(__position_estimator.relativizePoint(arrival_point));
+  };
 
 private:
-  /** JSON file which describes the fuzzy system */
-  static constexpr auto __FUZZY_JSON{"/usr/bin/fuzzy_system_v1.json"};
+  /** Arrival area [cm] */
+  float __ARRIVAL_AREA = 10.0;
   /** Thread status */
   bool __is_running;
   /** Thread navigation */
@@ -35,10 +68,20 @@ private:
   Headlights __headlights;
 	/** Distance sensor object */
 	DistanceSensors __distance_sensor;
-  /** Compass object */
-  Compass __compass;
-  /** Fuzzy control system object */
-  std::shared_ptr<FuzzyControlSystem> __fuzzy_system;
+  /** Position estimator object */
+  PositionEstimator __position_estimator;
+  /** Tracker object */
+  std::shared_ptr<Tracker> __tracker;
+  /** Evasion object */
+  SmartEvasion __evader;
+  /** Recognized obstacles */
+  std::vector<Obstacle> __obstacles;
+  /** Utilities */
+  utilities __utilities;
+  /** Lights control */
+  SmartLights __smart_lights;
+  /** Reporter object */
+  std::shared_ptr<Reporter> __reporter;
   /**
    * @brief Frame callback
    * 
@@ -51,7 +94,76 @@ private:
    * 
    */
   void __navigation();
-  
+  /**
+   * @brief Wait for the image processing module to recognize and 
+   * create the obstacle object
+   * 
+   * @param curr_robot_angle 
+   * @param distance_to_obstacle 
+   * @param curr_position 
+   */
+  void __visualization(float curr_robot_angle, float distance_to_obstacle, CartesianPosition curr_position);
+  /**
+   * @brief Check if the destination is reached
+   * 
+   * @param curr_position 
+   * @return true 
+   * @return false 
+   */
+  bool __arrivation(CartesianPosition curr_position, CartesianPosition current_target_position) {
+    bool ret;
+    float dx = std::fabs(curr_position.x - current_target_position.x);
+    float dy = std::fabs(curr_position.y - current_target_position.y);
+    if (dx < __ARRIVAL_AREA && dy < __ARRIVAL_AREA)
+      ret = true;
+    else 
+      ret = false;
+    return ret;
+  };
+  /**
+   * @brief Get the distance to the target.
+   * 
+   * @param curr_position 
+   * @return CartesianPosition : Relative target position to the current position
+   */
+  CartesianPosition __getRelativeTargetPos (CartesianPosition curr_position, CartesianPosition current_target_position) {
+    CartesianPosition ret;
+
+    ret.x = current_target_position.x - curr_position.x;
+    ret.y = current_target_position.y - curr_position.y;
+    return ret;
+  };
+  /**
+   * @brief Get the angle where i have to go to reach
+   * my destination.
+   * 
+   * @param rel_target_from_curr_pos Relative target position to the current position 
+   * @return float: Angle where i have to go. left [0:180], right [0:-180]
+   */
+  float whereHaveToGo(CartesianPosition rel_target_from_curr_pos) {
+    //ret.distance = std::sqrt(std::pow(dx, 2) + std::pow(dy, 2));
+    float angle = atan2(rel_target_from_curr_pos.y, rel_target_from_curr_pos.x) * 180.0 / M_PI;
+    if (angle > 180.0)
+      angle = angle - 360;
+    return angle;
+  };
+  /**
+   * @brief Get the Closest Position object
+   * 
+   * @param obstacle 
+   * @return PolarPosition 
+   */
+  PolarPosition getClosestPosition(Obstacle obstacle) {
+    PolarPosition closest;
+    PolarPosition obstacle_center = __position_estimator.cartensianToPolar(obstacle.getPosition());
+    PolarPosition obstacle_leftmost = __position_estimator.cartensianToPolar(obstacle.getLeftmostPoint());
+    PolarPosition obstacle_rightmost = __position_estimator.cartensianToPolar(obstacle.getRightmostPoint());
+    
+    closest = obstacle_center;
+    if (obstacle_leftmost.distance < closest.distance)
+    return closest;
+  }; 
+
 };
 
 #endif /* DBCD007B_2AE4_4807_BE39_8D3ADAFADDB8 */
